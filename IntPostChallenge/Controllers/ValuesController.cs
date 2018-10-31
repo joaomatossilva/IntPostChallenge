2fe15d0082787fc5de195a08e3adc1ffa5c92eee
Joao Matos Silva <kappy@acydburne.com.pt>
2018-10-31 21:45:26 +0000

    Add project files.

---
+++ IntPostChallenge/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IntPostChallenge.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntPostChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // POST api/values
+        [HttpPost]
+        public RequestModel Post([FromBody] RequestModel value)
+        {
+            return value;
+        }
+    }
+}
---
+++ IntPostChallenge/Model/RequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntPostChallenge.Model
+{
+    public class RequestModel
+    {
+        public int Number { get; set; }
+    }
+}
